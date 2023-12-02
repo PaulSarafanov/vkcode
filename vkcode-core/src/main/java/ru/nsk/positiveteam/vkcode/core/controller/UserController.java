cd1003ba03a1d75edabb9fdcb36cdd275609d0dd
Pavel Sarafanov <sarafanov.paul@gmail.com>
2023-12-02 20:56:18 +0700

    initial commit

--- vkcode-core/src/main/java/ru/nsk/positiveteam/vkcode/core/controller/UserController.java
+++ vkcode-core/src/main/java/ru/nsk/positiveteam/vkcode/core/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.nsk.positiveteam.vkcode.core.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.nsk.positiveteam.vkcode.api.dto.UserDto;
